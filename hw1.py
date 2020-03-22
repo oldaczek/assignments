5de2e82dd4bc43472ae39627c504fadc337ad263
oldaczek <48659281+oldaczek@users.noreply.github.com>
2020-03-22 12:50:33 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -28,8 +28,11 @@ def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     :return: Number of cases on a given date as an integer
     """
     
-    # Your code goes here (remove pass)
-    pass
+    d = datetime.date(year,month,day)
+    data = d.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    inpoland = confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"]
+    result = inpoland[data].values[0]
+    return result
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
@@ -48,8 +51,10 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     :return: A list of strings with the names of the coutires
     """
 
-    # Your code goes here (remove pass)
-    pass
+    d = datetime.date(year, month, day)
+    d1 = d.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    countries = confirmed_cases[["Country/Region", d1]].gropuby(["Country/Region"]).sum().sort_values(by=d1, ascending=False).head(5)
+    return list(countries.index)
 
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
@@ -68,6 +73,9 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     :param year: Month to get the countries for as an integer indexed from 1
     :return: Number of countries/regions where the count has not changed in a day
     """
+    d = datetime.date(year, month, day)
+    d1 = d.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    wczoraj = d - datetime.timedelta(days=1)
+    wczorajstr = wczoraj.strftime('%m/"%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    return len(confirmed_cases.loc[confirmed_cases[d1]-confirmed_cases[wczorajstr]!=0].index)
     
-    # Your code goes here (remove pass)
-    pass
